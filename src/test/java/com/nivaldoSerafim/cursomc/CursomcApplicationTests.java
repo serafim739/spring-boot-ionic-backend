19f85885d7a13a07ebb543659aefabfacb77032a
serafim739 <nyvalldo_shekina@hotmail.com.br>
2020-11-02 09:29:15 -0300

    Projeto criado

---
+++ src/test/java/com/nivaldoSerafim/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nivaldoSerafim.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
