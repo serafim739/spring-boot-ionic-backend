f414ae9d5d94718406479116e62ad29f6bfa5647
serafim739 <nyvalldo_shekina@hotmail.com.br>
2020-12-12 13:06:37 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/nivaldoSerafim/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/nivaldoSerafim/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,18 @@ package com.nivaldoSerafim.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 import com.nivaldoSerafim.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty
+	@Length(min=5, max=80, message="O tamanho deve ente 5 e 80 caracteres")
 	private String name;
 	
 	public CategoriaDTO() {
